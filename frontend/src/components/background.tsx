9157d35716cc0d18c358cde15902deb5a6dddf08
louis <louislim2005@gmail.com>
2025-08-09 02:36:55 +1000

    created simple background

---
+++ frontend/src/components/background.tsx
@@ -0,0 +1,10 @@
+const Background = () => {
+
+    return (
+        <div className="absolute inset-0 bg-[#6aa84f] w-full h-full -z-1">
+            
+        </div>
+    );
+};
+
+export default Background
